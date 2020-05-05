78428479cda1e00db7b8b6ca4d979aab242d2a1e
Yaroslav Kasperovich <ykasperovich@conceptant.com>
2020-05-05 18:41:08 +0300

    Add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,20 @@
 module.exports = {
   extends: ["airbnb-typescript-prettier"],
+  rules: {
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "**/*.test.js",
+          "**/*.test.jsx",
+          "**/*.test.ts",
+          "**/*.test.tsx",
+          "src/tests/**/*",
+          "**/*.stories.js",
+          "**/*.stories.ts",
+          "**/*.stories.tsx",
+        ],
+      },
+    ],
+  },
 };
