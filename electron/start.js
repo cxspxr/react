d05ba4fbbadf0a6dad72ebae64b573352d95ab13
Yaroslav Kasperovich <ykasperovich@conceptant.com>
2020-05-06 21:25:56 +0300

    Serve electron through express

--- electron/start.js
+++ electron/start.js
@@ -1,32 +1,43 @@
+/* eslint-disable */
 const { app, BrowserWindow } = require("electron");
 
 const path = require("path");
 const url = require("url");
 
+const express = require("express");
+const server = express();
+
+const port = process.env.PORT ? process.env.PORT - 200 : 8080;
+
+const createServer = () => {
+  server.use(express.static(path.join(__dirname, "../build")));
+  server.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/index.html"));
+  });
+  server.listen(port);
+};
+
 let mainWindow;
 
 const createWindow = () => {
+  if (!process.env.ELECTRON_START_URL) {
+    createServer();
+  }
+
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
     },
   });
 
-  mainWindow.loadURL(
-    process.env.ELECTRON_START_URL ||
-      url.format({
-        pathname: path.join(__dirname, "../build/index.html"),
-        protocol: "file:",
-        slashes: true,
-      })
-  );
+  mainWindow.loadURL(process.env.ELECTRON_START_URL || `http://localhost:${port}`);
 
   mainWindow.on("closed", () => {
-    mainWindow = null
+    mainWindow = null;
   });
-}
+};
 
 app.on("ready", createWindow);
 
